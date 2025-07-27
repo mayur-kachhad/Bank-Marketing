63882fbbcdc8b44651032e5c5f152bd2f604db1c
mayur kachhad <mayurkachhad022@gmail.com>
2025-07-27 11:40:12 +0530

    added setup.py

---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import find_packages, setup
+from typing import List
+
+HYPHEN_E_DOT = '-e .'
+
+def get_requirements(file_path: str) -> List[str]:
+    '''
+    This function returns a list of requirements from requirements.txt
+    instead of opening file manually we can also use pathlib liabrary
+    to open the file.
+    '''
+    requirements = []
+    with open(file_path) as file_obj:
+        requirements = file_obj.readlines()
+        requirements = [req.replace("\n", "") for req in requirements]
+
+        if HYPHEN_E_DOT in requirements:
+            requirements.remove(HYPHEN_E_DOT)
+
+    return requirements
+
+setup(
+    name="mlproject",
+    version="0.0.1",
+    author="Mayur",
+    author_email="mayurkachhad022@gmail.com",
+    packages=find_packages(),  
+    install_requires=get_requirements("requirements.txt")
+)
